4067c218da2ccac27b722d1a0bce73b1de5d17f7
jonghyekim <rebecakim22@gmail.com>
2026-05-23 23:41:32 +0900

    message notification

--- src/main/java/com/project/software_engineering/repository/ChatMessageRepository.java
+++ src/main/java/com/project/software_engineering/repository/ChatMessageRepository.java
@@ -3,9 +3,14 @@ package com.project.software_engineering.repository;
 import com.project.software_engineering.domain.ChatMessage;
 import com.project.software_engineering.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByChatRoomAndDeletedFalseOrderByCreatedAtAsc(ChatRoom chatRoom);
+
+    @Query("SELECT COUNT(m) FROM ChatMessage m WHERE m.chatRoom = :room AND m.deleted = false AND m.sender.id != :userId AND m.isRead = false")
+    long countUnread(@Param("room") ChatRoom room, @Param("userId") Long userId);
 }
